70f5dcd2c89298d750bee6a0941d25b1ad67ff7f
howhow <debby8698@gmail.com>
2023-04-18 11:44:26 +0800

    feat: add user model and seed

---
+++ database/seeders/20230417080608-users-seed-file.js
@@ -0,0 +1,44 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users',
+      [
+        {
+          name: 'user1',
+          account: 'user1',
+          password: bcrypt.hashSync(process.env.DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+          organization: 'the 5th class',
+          group: '5th',
+          role: 'user',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          name: 'user2',
+          account: 'user2',
+          password: bcrypt.hashSync(process.env.DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+          organization: 'the 6th class',
+          group: '6th',
+          role: 'user',
+          created_at: new Date(),
+          updated_at: new Date()
+        },
+        {
+          name: 'root',
+          account: 'root',
+          password: bcrypt.hashSync(process.env.DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+          organization: 'the 5th class',
+          group: '5th',
+          role: 'admin',
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
