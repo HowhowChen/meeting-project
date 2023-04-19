6be8e6fbec94e7bd4d8406be413db9e584a833f6
howhow <debby8698@gmail.com>
2023-04-19 10:01:05 +0800

    feat: add dependency package & test handlebars for bootstrap

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const exphbs = require('express-handlebars')
 const express = require('express')
+
+const routes = require('./routes')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
 require('./database/models')
+
+// template engine: express-handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+// static file
+app.use(express.static('public'))
+
+// middleware: routes
+app.use(routes)
 
 app.listen(PORT, () => console.log(`The app is listening ${PORT}`))
--- database/models/platform.js
+++ database/models/platform.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
-      Platform.hasMany('Meeting', { foreignKey: 'platformId' })
+      Platform.hasMany(models.Meeting, { foreignKey: 'platformId' })
     }
   }
   Platform.init({
