cd2e0a7845f092d51103862b8f81cc0607e7632c
howhow <debby8698@gmail.com>
2023-05-07 14:41:59 +0000

    refactor: modify form-datalist

--- public/javascripts/form-datalist.js
+++ public/javascripts/form-datalist.js
@@ -1,7 +1,24 @@
 const issue = document.querySelector('#issue')
 const issueId = document.querySelector('#issueId')
-const issueCategory = document.querySelector('#issue-category')
-const option = document.querySelector('#issue-category option')
+const issueMessage = document.querySelector('#issue-message')
+const options = document.querySelectorAll('datalist option')
+
 try {
-  issue.addEventListener('change', e => { console.log(issue.value) })
+  issue.addEventListener('change', e => {
+    let validation = false
+    for (let i = 0; i < options.length; i++) {
+      if (issue.value === options[i].value) {
+        issueId.value = options[i].dataset.value
+        validation = true
+      }
+    }
+    issueMessage.classList.remove('text-success', 'text-danger')
+    if (!validation) {
+      issueMessage.classList.add('text-danger')
+      issueMessage.innerText = '請輸入內建選項文字！'
+      return
+    }
+    issueMessage.classList.add('text-success')
+    issueMessage.innerText = '輸入正確！'
+  })
 } catch {}
