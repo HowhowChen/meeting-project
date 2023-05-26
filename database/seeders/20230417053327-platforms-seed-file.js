3d1ea636852228b66eeb33cdff038c4e7f4f84fc
howhow <debby8698@gmail.com>
2023-05-26 17:07:58 +0800

    fix: modify platform seeders

--- database/seeders/20230417053327-platforms-seed-file.js
+++ database/seeders/20230417053327-platforms-seed-file.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Platforms',
-      ['Zoom', 'Webex', '騰訊']
+      ['ZOOM', 'WEBEX', 'TENCENT']
         .map(item => ({
           name: item,
           created_at: new Date(),
