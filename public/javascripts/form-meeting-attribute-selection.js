cf6f78d8360ecd91d622880d7c6580b9272170ad
howhow <debby8698@gmail.com>
2023-08-01 19:06:16 +0800

    feat: add swich select or text mode with country and category on postSevenMeeting function

--- public/javascripts/form-meeting-attribute-selection.js
+++ public/javascripts/form-meeting-attribute-selection.js
@@ -1,28 +1,46 @@
+const countryTextBlock = document.querySelector('#countryTextBlock')
+const countrySelectBlock = document.querySelector('#countrySelectBlock')
 const countryText = document.querySelector('#countryText')
 const countrySelect = document.querySelector('#countrySelect')
 const countryIsTrue = document.querySelector('#countryIsTrue')
 const countryIsFalse = document.querySelector('#countryIsFalse')
+const country = document.querySelector('#country')
 
+const categoryTextBlock = document.querySelector('#categoryTextBlock')
+const categorySelectBlock = document.querySelector('#categorySelectBlock')
 const categoryText = document.querySelector('#categoryText')
 const categorySelect = document.querySelector('#categorySelect')
 const categoryIsTrue = document.querySelector('#categoryIsTrue')
 const categoryIsFalse = document.querySelector('#categoryIsFalse')
+const category = document.querySelector('#category')
 
 try {
+  // window load
+  window.addEventListener('load', () => {
+    country.value = countrySelect.value
+    category.value = categorySelect.value
+    countryTextBlock.classList.add('hidden')
+    categoryTextBlock.classList.add('hidden')
+  })
+
   // country is true
   countryIsTrue.addEventListener('change', e => {
     e.target.checked = true
     countryIsFalse.checked = false
+    country.value = countrySelect.value
 
-    countryText.classList.add('hide')
-    countrySelect.classList.add('hide')
+    countryText.required = false
+    countrySelect.required = true
+
+    countryTextBlock.classList.add('hide')
+    countrySelectBlock.classList.add('hide')
     setTimeout(() => {
-      countrySelect.classList.remove('hidden')
-      countryText.classList.add('hidden')
+      countrySelectBlock.classList.remove('hidden')
+      countryTextBlock.classList.add('hidden')
     }, 300)
     setTimeout(() => {
-      countryText.classList.remove('hide')
-      countrySelect.classList.remove('hide')
+      countryTextBlock.classList.remove('hide')
+      countrySelectBlock.classList.remove('hide')
     }, 300)
   })
   // country is false
@@ -30,32 +48,49 @@ try {
     e.target.checked = true
     countryIsTrue.checked = false
 
-    countryText.classList.add('hide')
-    countrySelect.classList.add('hide')
+    countryText.required = true
+    countrySelect.required = false
+
+    countryTextBlock.classList.add('hide')
+    countrySelectBlock.classList.add('hide')
     setTimeout(() => {
-      countryText.classList.remove('hidden')
-      countrySelect.classList.add('hidden')
+      countryTextBlock.classList.remove('hidden')
+      countrySelectBlock.classList.add('hidden')
     }, 300)
     setTimeout(() => {
-      countryText.classList.remove('hide')
-      countrySelect.classList.remove('hide')
+      countryTextBlock.classList.remove('hide')
+      countrySelectBlock.classList.remove('hide')
     }, 300)
+  })
+
+  // country select on change
+  countrySelect.addEventListener('change', e => {
+    country.value = e.target.value
+  })
+
+  // country text on change
+  countryText.addEventListener('change', e => {
+    country.value = e.target.value
   })
 
   // category is true
   categoryIsTrue.addEventListener('change', e => {
     e.target.checked = true
     categoryIsFalse.checked = false
+    category.value = categorySelect.value
 
-    categoryText.classList.add('hide')
-    categorySelect.classList.add('hide')
+    categoryText.required = false
+    categorySelect.required = true
+
+    categoryTextBlock.classList.add('hide')
+    categorySelectBlock.classList.add('hide')
     setTimeout(() => {
-      categorySelect.classList.remove('hidden')
-      categoryText.classList.add('hidden')
+      categorySelectBlock.classList.remove('hidden')
+      categoryTextBlock.classList.add('hidden')
     }, 300)
     setTimeout(() => {
-      categoryText.classList.remove('hide')
-      categorySelect.classList.remove('hide')
+      categoryTextBlock.classList.remove('hide')
+      categorySelectBlock.classList.remove('hide')
     }, 300)
   })
   // category is false
@@ -63,15 +98,28 @@ try {
     e.target.checked = true
     categoryIsTrue.checked = false
 
-    categoryText.classList.add('hide')
-    categorySelect.classList.add('hide')
+    categoryText.required = false
+    categorySelect.required = true
+
+    categoryTextBlock.classList.add('hide')
+    categorySelectBlock.classList.add('hide')
     setTimeout(() => {
-      categoryText.classList.remove('hidden')
-      categorySelect.classList.add('hidden')
+      categoryTextBlock.classList.remove('hidden')
+      categorySelectBlock.classList.add('hidden')
     }, 300)
     setTimeout(() => {
-      categoryText.classList.remove('hide')
-      categorySelect.classList.remove('hide')
+      categoryTextBlock.classList.remove('hide')
+      categorySelectBlock.classList.remove('hide')
     }, 300)
+
+    // category select on change
+    categorySelect.addEventListener('change', e => {
+      category.value = e.target.value
+    })
+
+    // category text on change
+    categoryText.addEventListener('change', e => {
+      category.value = e.target.value
+    })
   })
 } catch {}
